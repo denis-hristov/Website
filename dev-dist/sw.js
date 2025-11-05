044285bc194ad800f7199f26dba8f6ad7f6b6876
Denis Hristov <denis.hristov145@gmail.com>
2025-11-05 22:26:07 +0200

    Fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.us59fe2pgj"
+    "revision": "0.2ukm9ek56ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
